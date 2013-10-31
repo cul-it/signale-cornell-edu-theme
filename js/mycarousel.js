292f9c26b2e5a3cad51584b72613656af63e00db
jrc88 <USER@DOMAIN.COM>
2013-10-31 15:37:29 +0000

    Type commit message here…
    git-svn-id: https://svn.library.cornell.edu/signaletheme/trunk@208 fd91eecc-2661-4fc6-be0c-6e8cf4f2035b

--- js/mycarousel.js
+++ js/mycarousel.js
@@ -1,22 +1,22 @@
 function mycarousel_initCallback(carousel) {
-	jQuery('.jacoursel-control a').bind('click',function(){
+	jQuery('.jcarousel-control a').bind('click',function(){
 		carousel.scroll(jQuery.jcarousel.intval(jQuery(this).text()));
 		return false;
 	});
 
-	jQuery('#mycarousel-next').bind('click',function() {
+	jQuery('.jcarousel-next').bind('click',function() {
 	carousel.next();
 	return false;
 	});
 
-	jQuery('#mycarousel-prev').bind('click', function(){
+	jQuery('.jcarousel-prev').bind('click', function(){
 	carousel.prev();
 	return false;
 	});
 }
 
 jQuery(document).ready(function() {
-	jQuery("#mycarousel").jcarousel({
+	jQuery(".jcarousel-skin-default").jcarousel({
 		scroll:1,
 		initCallback: mycarousel_initCallback,
 		buttonNextHTML: null,
