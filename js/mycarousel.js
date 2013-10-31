e12522dc7747f1bff5da27b8648891bcb692c6a1
jrc88 <USER@DOMAIN.COM>
2013-10-31 14:54:39 +0000

    Type commit message here…
    git-svn-id: https://svn.library.cornell.edu/signaletheme/trunk@206 fd91eecc-2661-4fc6-be0c-6e8cf4f2035b

---
+++ js/mycarousel.js
@@ -0,0 +1,25 @@
+function mycarousel_initCallback(carousel) {
+	jQuery('.jacoursel-control a').bind('click',function(){
+		carousel.scroll(jQuery.jcarousel.intval(jQuery(this).text()));
+		return false;
+	});
+
+	jQuery('#mycarousel-next').bind('click',function() {
+	carousel.next();
+	return false;
+	});
+
+	jQuery('#mycarousel-prev').bind('click', function(){
+	carousel.prev();
+	return false;
+	});
+}
+
+jQuer(document).ready(function() {
+	jQuery("#mycarousel").jcarousel({
+		scroll:1,
+		initCallback: mycarousel_initCallback,
+		buttonNextHTML: null,
+		buttonPrevHTML:null
+	});
+})
