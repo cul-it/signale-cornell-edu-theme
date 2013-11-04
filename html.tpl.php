9c2dc949c3262daa2ce7900ad3e23091b94c53c7
mhk33 <USER@DOMAIN.COM>
2013-11-04 19:42:26 +0000

    debug js
    git-svn-id: https://svn.library.cornell.edu/signaletheme/trunk@323 fd91eecc-2661-4fc6-be0c-6e8cf4f2035b

--- html.tpl.php
+++ html.tpl.php
@@ -96,10 +96,10 @@
   <?php print $page; ?>
   <?php print $page_bottom; ?>
   <script type="text/javascript">
-    $(document).ready(function() { 
+    jQuery(document).ready(function() { 
     var randomImages = ['etching1','etching2','etching3','etching4']; 
     var rndNum = Math.floor(Math.random() * randomImages.length); 
-    $(".hero").css({ background: "url('/sites/all/themes/signale/images/hero/" + randomImages[rndNum] + ".jpg') no-repeat" }); });
+    jQuery(".hero").css({ background: "url('/sites/all/themes/signale/images/hero/" + randomImages[rndNum] + ".jpg') no-repeat" }); });
   </script>
 </body>
 </html>
