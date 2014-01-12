9c5d3103d2c1f118c48f6fef98b6dd8d9dd12187
mhk33 <USER@DOMAIN.COM>
2014-01-12 23:22:00 +0000

    add js for tooltip
    git-svn-id: https://svn.library.cornell.edu/signaletheme/trunk@349 fd91eecc-2661-4fc6-be0c-6e8cf4f2035b

--- html.tpl.php
+++ html.tpl.php
@@ -95,5 +95,10 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  <script>
+    (function($) {
+      $('a').tooltip-link();
+      })(jQuery);
+  </script>
 </body>
 </html>
