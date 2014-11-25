cc88fef1793352543acc078a021835e066a0bd77
mhk33 <USER@DOMAIN.COM>
2014-11-25 15:34:54 +0000

    add more fields
    git-svn-id: https://svn.library.cornell.edu/signaletheme/trunk@369 fd91eecc-2661-4fc6-be0c-6e8cf4f2035b

--- node--audio.tpl.php
+++ node--audio.tpl.php
@@ -12,7 +12,34 @@
   <?php if (!empty ($content['field_audio_book'])) : ?>
     <h3><?php print render($content['field_audio_book']); ?></h3>
   <?php endif; ?>
-  <?php if (!empty ($content['field_book_node_id'])) : ?>
-    <span class="audio-book-link"><a href="/books/<?php print render($content['field_book_node_id']); ?>">Book detail</a></span>
+</div>
+
+<div class="audio-speaker">
+  <?php if (!empty ($content['field_audio_speaker'])) : ?>
+    <?php print render($content['field_audio_speaker']); ?>
   <?php endif; ?>
+</div>
+
+<div class="audio-data">
+
+  <?php if (!empty ($content['field_audio2_date'])) : ?>
+    <div class="audio-date"><?php print render($content['field_audio2_date']); ?></div>
+  <?php endif; ?>
+
+  <?php if (!empty ($content['field_audio_context'])) : ?>
+    <div class="audio-context"><?php print render($content['field_audio_context']); ?></div>
+  <?php endif; ?>
+
+  <?php if (!empty ($content['field_audio_language'])) : ?>
+    <div class="audio-language"><?php print render($content['field_audio_language']); ?></div>
+  <?php endif; ?>
+
+  <?php if (!empty ($content['field_audio_rights'])) : ?>
+    <div class="audio-rights"><?php print render($content['field_audio_rights']); ?></div>
+  <?php endif; ?>
+
+  <?php if (!empty ($content['body'])) : ?>
+    <?php print render($content['body']); ?>
+  <?php endif; ?>
+
 </div>
