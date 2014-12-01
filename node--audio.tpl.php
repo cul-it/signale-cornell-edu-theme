de41fa9dd6593e04866ecfce9278197746202fda
mb863 <USER@DOMAIN.COM>
2014-12-01 17:42:21 +0000

    working on multimedia page
    git-svn-id: https://svn.library.cornell.edu/signaletheme/trunk@378 fd91eecc-2661-4fc6-be0c-6e8cf4f2035b

--- node--audio.tpl.php
+++ node--audio.tpl.php
@@ -10,7 +10,7 @@
 
 <div class="audio-book">
   <?php if (!empty ($content['field_audio_book'])) : ?>
-    <h3><?php print render($content['field_audio_book']); ?></h3>
+    <h3><?php print render($content['field_audio_book']); ?> <a href="#">Book Detail</a></h3>
   <?php endif; ?>
 </div>
 
@@ -29,7 +29,8 @@
 <div class="audio-data">
 
   <?php if (!empty ($content['field_audio2_date']) || ($content['field_audio_duration'])) : ?>
-    <div class="audio-date"><?php print render($content['field_audio2_date']); ?> / <?php print render($content['field_audio_duration']); ?></div>
+    <div class="audio-date"><?php print render($content['field_audio2_date']); ?>
+      <?php print render($content['field_audio_duration']); ?></div>
   <?php endif; ?>
 
   <?php if (!empty ($content['field_audio_context'])) : ?>
