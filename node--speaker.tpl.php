42229393c9a96dcc6f7698f7b3c433eadcb0f6d6
mb863 <USER@DOMAIN.COM>
2014-12-01 18:12:31 +0000

    styling multimedia pages
    git-svn-id: https://svn.library.cornell.edu/signaletheme/trunk@381 fd91eecc-2661-4fc6-be0c-6e8cf4f2035b

--- node--speaker.tpl.php
+++ node--speaker.tpl.php
@@ -10,18 +10,15 @@
 
 
 
-<div class="row">
-  <div class="span2">
+
     <?php if (!empty ($content['field_speaker_photo'])) : ?>
       <?php print render($content['field_speaker_photo']); ?>
     <?php endif; ?>
-  </div>
-  <div class="span10">
+
     <?php if (!$page): ?>
       <h3><?php print $title_attributes; ?><?php print $title; ?></h3>
     <?php endif; ?>
     <?php if (!empty ($content['body'])) : ?>
       <?php print render($content['body']); ?>
     <?php endif; ?>
-  </div>
-</div>
+
