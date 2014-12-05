8da08598ebcc09be0a7d1f4efb5fecd74e764b84
jgr25 <USER@DOMAIN.COM>
2014-12-05 22:24:37 +0000

    simpleify book id
    git-svn-id: https://svn.library.cornell.edu/signaletheme/trunk@425 fd91eecc-2661-4fc6-be0c-6e8cf4f2035b

--- node--audio.tpl.php
+++ node--audio.tpl.php
@@ -58,7 +58,6 @@
 
 <p>
   <?php
-  echo 'field_audio_book: ' . print_r($content['field_audio_book'], TRUE);
-  echo 'field_book_node_id: ' . print_r($content['field_book_node_id'], TRUE);
+  echo 'field_book_node_id: ' . print_r(array_pop(array_pop($content['field_book_node_id'])), TRUE);
   ?>
 </p>
