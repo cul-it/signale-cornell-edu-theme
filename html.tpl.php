fb93f212c997629507abefe594707b452e93a22c
mkwallace <melissawallace@Melissas-MacBook-Pro-3.local>
2015-08-24 15:54:51 -0400

    add piwik code

--- html.tpl.php
+++ html.tpl.php
@@ -100,5 +100,21 @@
       $('a.tooltip-link').tooltip();
       })(jQuery);
   </script>
+  <!-- Piwik -->
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//webanalytics.library.cornell.edu/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', 511]);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <noscript><p><img src="//webanalytics.library.cornell.edu/piwik.php?idsite=511" style="border:0;" alt="" /></p></noscript>
+  <!-- End Piwik Code -->
+
 </body>
 </html>
